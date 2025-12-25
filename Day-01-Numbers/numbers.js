158e162ddd12fee7e5496948d16dbe0ad6a87424
Tayyab Ali <freelancewithtayyab@gmail.com>
2025-12-25 22:55:12 +0500

    `Added function to print numbers from N to 1, updated existing function to parse input as integer`

--- Day-01-Numbers/numbers.js
+++ Day-01-Numbers/numbers.js
@@ -1,9 +1,21 @@
 // PROBLEM 1  PRINT NUMBER FROM 1 TO N
 
 function printNum() {
-    const promptNum = prompt('Enter your number')
+    const promptNum = parseInt(prompt('Enter your number'))
     for (let index = 1; index <= promptNum; index++) {
         console.log(index)
     }
 }
-printNum()
+// printNum()
+
+// PROBLEM 2 PRINT NUM FROM N TO 1 WITHOUT CHANGING THE LOOP CONDITION OF ABOVE QUESTION
+
+function printNumSecondVer() {
+    const promptNum = parseInt(prompt('Enter your number'))
+    for (let index = 1; index <= promptNum; index++) {
+        console.log(promptNum - index + 1)
+    }
+}
+// printNumSecondVer()
+
+
