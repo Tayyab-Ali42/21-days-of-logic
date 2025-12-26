e7e623687d5494bf9f3ce6d76025905b6e3ef549
Tayyab Ali <freelancewithtayyab@gmail.com>
2025-12-26 23:33:01 +0500

    `Added new function checkNum to determine if a number is positive, negative or zero`

--- Day-02-Conditions/Conditions.js
+++ Day-02-Conditions/Conditions.js
@@ -11,3 +11,14 @@ let findMax = (a, b, c) => {
     return max
 }
 console.log(findMax(5, 29, 30))
+
+// PROBLEM 2 CHECK IF A NUM IS A POSITIVE, NEGATIVE OR ZERO
+
+let checkNum = (num = 0) => {
+    if (num < 0) return 'Number is a Negative'
+    if (num > 0) return 'Number is a positive'
+    return 'Number is a Zero'
+}
+console.log(checkNum(10))
+console.log(checkNum(-8))
+console.log(checkNum(0))
